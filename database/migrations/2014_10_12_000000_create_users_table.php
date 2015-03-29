dbd65590590884ca91e90bd0c372c781ac2f99e6
Nattaphat K.@home <nattaphat@gmail.com>
2015-03-29 21:10:31 +0700

    Edit database config and alter user table template

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,20 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name');
+            $table->integer('user_role_id');
+            $table->integer('salary_id');
+            $table->string('username');
+            $table->string('password', 60);
+            $table->string('firstname');
+            $table->string('lastname');
 			$table->string('email')->unique();
-			$table->string('password', 60);
+            $table->string('telephone');
+            $table->date('birthday');
+            $table->string('sex',15);
+            $table->text('address');
+            $table->string('occupation');
+            $table->boolean('receive_news');
+            $table->string('signup_type');
 			$table->rememberToken();
 			$table->timestamps();
 		});
