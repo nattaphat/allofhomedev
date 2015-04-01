e7f6afecc4dc7281f4a15928f59027213e8e81d8
Yotzapon <yotsapon@haii.or.th>
2015-04-01 16:33:21 +0700

    Add template table

---
+++ database/migrations/2015_04_01_040612_creat_vip_current_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatVipCurrentTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('vip_current', function(Blueprint $table)
+        {
+            $table->increments('id');
+//            $table->primary('id');
+            $table->integer('user_id')->nullable(false);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->dateTime('from_date')->nullable(false);
+            $table->dateTime('to_date')->nullable(false);
+            $table->dateTime('payment_datetime')->nullable(false);
+            $table->float('payment_amount')->nullable(false);
+            $table->string('payment_type')->nullable(false);
+            $table->string('remark')->nullable(false);
+            $table->integer('attachment_id')->nullable(true);
+            $table->foreign('attachment_id')->references('id')->on('attachment');
+            $table->integer('vip_feature')->nullable(false);
+            $table->integer('update_by')->nullable(false);
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('vip_current');
+	}
+
+}
---
+++ database/migrations/2015_04_01_092601_create_branch_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBranchTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('branch', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('shop_id')->nullable(false);
+            $table->foreign('shop_id')->references('id')->on('shop');
+            $table->string('branch_name')->nullable(false);
+            $table->string('telephone')->nullable(true);
+            $table->string('mobile_phone')->nullable(false);
+            $table->string('fax')->nullable(true);
+            $table->string('email')->nullable(false);
+            $table->string('service_day')->nullable(true);
+            $table->string('service_time')->nullable(true);
+            $table->boolean('credit_card')->nullable(true);
+            $table->boolean('parking')->nullable(true);
+            $table->string('lat')->nullable(false);
+            $table->string('long')->nullable(false);
+            $table->string('add_no')->nullable(false);
+            $table->string('add_building')->nullable(false);
+            $table->string('add_floor')->nullable(false);
+            $table->string('add_street')->nullable(false);
+            $table->integer('tambon_id')->nullable(false);
+//            $table->foreign('tambon_id')->references('tambid')->on('geo_tambon');
+            $table->integer('amphoe_id')->nullable(false);
+//            $table->foreign('amphoe_id')->references('amphid')->on('geo_amphoe');
+            $table->integer('provice_id')->nullable(false);
+//            $table->foreign('provice_id')->references('provid')->on('geo_province');
+            $table->integer('region_id')->nullable(false);
+            $table->foreign('region_id')->references('id')->on('geo_region');
+            $table->integer('area_id')->nullable(true);
+            $table->foreign('area_id')->references('id')->on('area');
+            $table->integer('subarea_id')->nullable(true);
+            $table->foreign('subarea_id')->references('id')->on('subarea');
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schmema::drop('branch');
+	}
+
+}
