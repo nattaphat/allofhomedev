1e6ae9c9f6c68476bd7dba2547b28e6a52253678
thapakit sarakham <thapakit@gmail.com>
2015-05-16 17:27:08 +0700

    left_menu

--- resources/views/layouts/_partials/left_menu.blade.php
+++ resources/views/layouts/_partials/left_menu.blade.php
@@ -1,32 +1,64 @@
-<div class="row" >
-    <div class="col-md-2"></div>
-    <div class="col-md-10 panel panel-default">
-        <div class="panel-body">
-            <ul class="nav nav-sidebar">
-                <li>
-                    <a href="{{ URL::to('home_index') }}">โครงการบ้านใหม่</a>
-                    <a href="{{ URL::to('townhome_index') }}">โครงการทาวน์โฮมใหม่</a>
-                    <a href="{{ URL::to('condo_index') }}">โครงการคอนโดใหม่</a>
-                    <a href="{{ URL::to('interior_index') }}">ออกแบบภายใน ภายนอก</a>
-                    <a href="{{ URL::to('land_index') }}">ที่ดินเปล่า</a>
-                    <a href="{{ URL::to('furniture_index') }}">เฟอร์นิเจอร์ ของตกแต่ง</a>
-                    <a href="{{ URL::to('electric_index') }}">เครื่องใช้ไฟฟ้า</a>
-                    <a href="{{ URL::to('kitchenware_index') }}">เครื่องครัว สุขภัณฑ์</a>
-                    <a href="{{ URL::to('contractor_index') }}">วัสดุก่อสร้าง รับเหมา</a>
-                    <a href="{{ URL::to('garden_index') }}">ดูแลสวน</a>
-                    <a href="{{ URL::to('old_furniture_index') }}">เฟอร์โบราณเก่าเก็บ</a>
-                </li>
-            </ul>
-        </div>
+{{--<div class="row" >--}}
+    {{--<div class="col-md-2"></div>--}}
+    {{--<div class="col-md-10 panel panel-default">--}}
+        {{--<div class="panel-body">--}}
+            {{--<ul class="nav nav-sidebar">--}}
+                {{--<li>--}}
+                    {{--<a href="{{ URL::to('home_index') }}">โครงการบ้านใหม่</a>--}}
+                    {{--<a href="{{ URL::to('townhome_index') }}">โครงการทาวน์โฮมใหม่</a>--}}
+                    {{--<a href="{{ URL::to('condo_index') }}">โครงการคอนโดใหม่</a>--}}
+                    {{--<a href="{{ URL::to('interior_index') }}">ออกแบบภายใน ภายนอก</a>--}}
+                    {{--<a href="{{ URL::to('land_index') }}">ที่ดินเปล่า</a>--}}
+                    {{--<a href="{{ URL::to('furniture_index') }}">เฟอร์นิเจอร์ ของตกแต่ง</a>--}}
+                    {{--<a href="{{ URL::to('electric_index') }}">เครื่องใช้ไฟฟ้า</a>--}}
+                    {{--<a href="{{ URL::to('kitchenware_index') }}">เครื่องครัว สุขภัณฑ์</a>--}}
+                    {{--<a href="{{ URL::to('contractor_index') }}">วัสดุก่อสร้าง รับเหมา</a>--}}
+                    {{--<a href="{{ URL::to('garden_index') }}">ดูแลสวน</a>--}}
+                    {{--<a href="{{ URL::to('old_furniture_index') }}">เฟอร์โบราณเก่าเก็บ</a>--}}
+                {{--</li>--}}
+            {{--</ul>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
+
+{{--<div class="row">--}}
+    {{--<div class="col-md-2"></div>--}}
+    {{--<div class="col-md-10 panel panel-default">--}}
+        {{--<div class="panel-body">--}}
+            {{--<i class="fa fa-facebook-official fa-3x"></i> &nbsp;&nbsp; <a href="#"> Like Facebook</a>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
+
+<div style="padding-left: 20px;">
+    <div class="list-group">
+        <a href="{{ URL::to('home_index') }}"
+           class="list-group-item list-group-item-success">โครงการบ้านใหม่</a>
+        <a href="{{ URL::to('townhome_index') }}"
+           class="list-group-item list-group-item-success">โครงการทาวน์โฮมใหม่</a>
+        <a href="{{ URL::to('condo_index') }}"
+           class="list-group-item list-group-item-success">โครงการคอนโดใหม่</a>
+        <a href="{{ URL::to('interior_index') }}"
+           class="list-group-item list-group-item-success">ออกแบบภายใน ภายนอก</a>
+        <a href="{{ URL::to('land_index') }}"
+           class="list-group-item list-group-item-success">ที่ดินเปล่า</a>
+        <a href="{{ URL::to('furniture_index') }}"
+           class="list-group-item list-group-item-success">เฟอร์นิเจอร์ ของตกแต่ง</a>
+        <a href="{{ URL::to('electric_index') }}"
+           class="list-group-item list-group-item-success">เครื่องใช้ไฟฟ้า</a>
+        <a href="{{ URL::to('kitchenware_index') }}"
+           class="list-group-item list-group-item-success">เครื่องครัว สุขภัณฑ์</a>
+        <a href="{{ URL::to('contractor_index') }}"
+           class="list-group-item list-group-item-success">วัสดุก่อสร้าง รับเหมา</a>
+        <a href="{{ URL::to('garden_index') }}"
+           class="list-group-item list-group-item-success">ดูแลสวน</a>
+        <a href="{{ URL::to('old_furniture_index') }}"
+           class="list-group-item list-group-item-success">เฟอร์โบราณเก่าเก็บ</a>
+    </div>
+
+    <div class="form-group">
+        <a class="btn btn-block btn-social btn-facebook">
+            <i class="fa fa-facebook"></i> Like Facebook
+        </a>
     </div>
 </div>
-
-<div class="row">
-    <div class="col-md-2"></div>
-    <div class="col-md-10 panel panel-default">
-        <div class="panel-body">
-            <i class="fa fa-facebook-official fa-3x"></i> &nbsp;&nbsp; <a href="#"> Like Facebook</a>
-        </div>
-    </div>
-</div>
-
