4c911c1ac418b3de0ad187f3f160ce1446b2befb
thapakit sarakham <thapakit@gmail.com>
2015-05-29 19:33:13 +0700

    modify view page, home townhome condo

--- app/Models/AllFunction.php
+++ app/Models/AllFunction.php
@@ -102,7 +102,7 @@ class AllFunction {
     public static function convertYoutube($string) {
         $iframe = preg_replace(
             "/\s*[a-zA-Z\/\/:\.]*youtu(be.com\/watch\?v=|.be\/)([a-zA-Z0-9\-_]+)([a-zA-Z0-9\/\*\-\_\?\&\;\%\=\.]*)/i",
-            "<iframe width=\"560\" height=\"315\" src=\"//www.youtube.com/embed/$2\" allowfullscreen></iframe>",
+            "<iframe src=\"//www.youtube.com/embed/$2\" allowfullscreen></iframe>",
             $string
         );
 
