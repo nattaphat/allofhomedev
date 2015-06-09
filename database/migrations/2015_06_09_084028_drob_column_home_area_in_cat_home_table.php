3d05ffbc73185d82a85a3ecfbeb840c9e9fba6b3
thapakit sarakham <thapakit@gmail.com>
2015-06-09 15:43:35 +0700

    modify table cat_home

---
+++ database/migrations/2015_06_09_084028_drob_column_home_area_in_cat_home_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DrobColumnHomeAreaInCatHomeTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('cat_home', function(Blueprint $table)
+		{
+            $table->dropColumn(['home_area']);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('cat_home', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
---
+++ database/migrations/2015_06_09_084211_all_column_home_area_in_cat_home_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AllColumnHomeAreaInCatHomeTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('cat_home', function(Blueprint $table)
+		{
+			$table->text('home_area')->nullable(true);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('cat_home', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
