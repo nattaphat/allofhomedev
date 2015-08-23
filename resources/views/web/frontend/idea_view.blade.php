b9295b0a2cf8c1f06542442429ddc200e3600813
thapakit sarakham <thapakit@gmail.com>
2015-08-23 17:06:25 +0700

    comment facebook each page

--- resources/views/web/frontend/idea_view.blade.php
+++ resources/views/web/frontend/idea_view.blade.php
@@ -243,7 +243,7 @@
             <p class="text">ฝากกด like และ share เพื่อเป็นกำลังใจเจ้าของกระทู้ด้วยนะคะ</p>
         </div>
         <div class="comment-fb">
-            <div class="fb-comments" data-href="https://www.facebook.com/allofhome" data-width="873"
+            <div class="fb-comments" data-href="{{ Request::fullUrl() }}" data-width="873"
                  data-numposts="10" data-order-by="reverse_time"></div>
         </div>
     </div>
