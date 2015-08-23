201bdc4b09e5258a459d3d64521c20f50d4bb344
thapakit sarakham <thapakit@gmail.com>
2015-08-23 18:36:07 +0700

    comment facebook each page

--- resources/views/layouts/main_view.blade.php
+++ resources/views/layouts/main_view.blade.php
@@ -22,7 +22,7 @@
 
     @yield('jshome')
 
-    <meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
+    <meta property="og:url"                content="{{ Request::fullUrl() }}" />
     <meta property="og:type"               content="article" />
     <meta property="og:title"              content="When Great Minds Don’t Think Alike" />
     <meta property="og:description"        content="How much does culture influence creative thinking?" />
