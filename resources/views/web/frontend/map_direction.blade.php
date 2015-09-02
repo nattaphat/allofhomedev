20cf0e0dcb9a65cf29428baee6bf4a3d49f4b3ce
thapakit sarakham <thapakit@gmail.com>
2015-09-02 08:02:35 +0700

    map direction

--- resources/views/web/frontend/map_direction.blade.php
+++ resources/views/web/frontend/map_direction.blade.php
@@ -90,47 +90,63 @@
                 }
             }
 
+            if ((navigator.platform.indexOf("iPhone") !== -1) || (navigator.platform.indexOf("iPod") !== -1)) {
+                function iOSversion() {
+                    if (/iP(hone|od|ad)/.test(navigator.platform)) {
+                        // supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
+                        var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+                        return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+                    }
+                }
+                var ver = iOSversion() || [0];
 
+                if (ver[0] >= 6) {
+                    protocol = 'maps://';
+                } else {
+                    protocol = 'http://';
 
-
-
-
-
-            // Check for geolocation support
-            if (navigator.geolocation) {
-                debugger;
-                navigator.geolocation.getCurrentPosition(function (position) {
-                            debugger;
-                            // Success!
-                            createMap({
-                                coords : true,
-                                lat : position.coords.latitude,
-                                lng : position.coords.longitude
-                            });
-                        },
-                        function () {
-                            debugger;
-                            // Gelocation fallback: Defaults to Stockholm, Sweden
-                            createMap({
-//                                coords : false,
-//                                address : "Sveavägen, Stockholm"
-                                coords : true,
-                                lat : "13.7650101",
-                                lng : "100.5382141"
-                            });
-                        }
-                );
+                }
+                window.location = protocol + 'maps.apple.com/maps?daddr=' + {{ $lat }} + ',' + {{ $long }} + '&amp;ll=';
             }
             else {
-                debugger;
-                // No geolocation fallback: Defaults to Lisbon, Portugal
-                createMap({
+//                window.open('http://maps.google.com?daddr=' + lat + ',' + lng + '&amp;ll=');
+
+                // Check for geolocation support
+                if (navigator.geolocation) {
+                    debugger;
+                    navigator.geolocation.getCurrentPosition(function (position) {
+                                debugger;
+                                // Success!
+                                createMap({
+                                    coords : true,
+                                    lat : position.coords.latitude,
+                                    lng : position.coords.longitude
+                                });
+                            },
+                            function () {
+                                debugger;
+                                // Gelocation fallback: Defaults to Stockholm, Sweden
+                                createMap({
+//                                coords : false,
+//                                address : "Sveavägen, Stockholm"
+                                    coords : true,
+                                    lat : "13.7650101",
+                                    lng : "100.5382141"
+                                });
+                            }
+                    );
+                }
+                else {
+                    debugger;
+                    // No geolocation fallback: Defaults to Lisbon, Portugal
+                    createMap({
 //                    coords : false,
 //                    address : "Lisbon, Portugal"
-                    coords : true,
-                    lat : "13.7650101",
-                    lng : "100.5382141"
-                });
+                        coords : true,
+                        lat : "13.7650101",
+                        lng : "100.5382141"
+                    });
+                }
             }
         });
     </script>
