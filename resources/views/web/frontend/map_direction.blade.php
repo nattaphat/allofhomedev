60e0118e75e960c3758c7f4f7d87d747414def1e
thapakit sarakham <thapakit@gmail.com>
2015-09-02 07:57:56 +0700

    map direction

--- resources/views/web/frontend/map_direction.blade.php
+++ resources/views/web/frontend/map_direction.blade.php
@@ -34,6 +34,9 @@
 
     <script type="text/javascript">
         $( document ).ready(function() {
+
+            alert(navigator.platform);
+
             var directionsService = new google.maps.DirectionsService(),
                     directionsDisplay = new google.maps.DirectionsRenderer(),
                     createMap = function (start) {
@@ -61,6 +64,37 @@
                             }
                         });
                     };
+
+            function navigate(lat, lng) {
+                // If it's an iPhone..
+                if ((navigator.platform.indexOf("iPhone") !== -1) || (navigator.platform.indexOf("iPod") !== -1)) {
+                    function iOSversion() {
+                        if (/iP(hone|od|ad)/.test(navigator.platform)) {
+                            // supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
+                            var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+                            return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+                        }
+                    }
+                    var ver = iOSversion() || [0];
+
+                    if (ver[0] >= 6) {
+                        protocol = 'maps://';
+                    } else {
+                        protocol = 'http://';
+
+                    }
+                    window.location = protocol + 'maps.apple.com/maps?daddr=' + lat + ',' + lng + '&amp;ll=';
+                }
+                else {
+                    window.open('http://maps.google.com?daddr=' + lat + ',' + lng + '&amp;ll=');
+                }
+            }
+
+
+
+
+
+
 
             // Check for geolocation support
             if (navigator.geolocation) {
