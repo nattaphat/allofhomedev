ec0c7adea259de6fd9db7088574b96856a21c4bf
thapakit sarakham <thapakit@gmail.com>
2015-09-09 20:35:21 +0700

    footer

--- resources/views/layouts/_partials/footer_new.blade.php
+++ resources/views/layouts/_partials/footer_new.blade.php
@@ -5,11 +5,11 @@
             <h2>ติดต่อเรา</h2>
             <p class="tel">087-998-8860</p>
             <p class="mail">info@allofhome.com</p>
-            <p class="address">Sunshine House, Sunville. SUN12 8LU.</p>
+            <p class="address">www.facebook.com/allofhome</p>
         </div>
         <div class="about">
             <h2>เกี่ยวกับเรา</h2>
-            <p>Making the web a prettier place one template at a time! We make beautiful, quality, responsive Drupal & web templates!</p>
+            <p>แหล่งรวม "รีวิว"เกี่ยวกับการตกแต่ง เฟอร์นิเจอร์ รับเหมาก่อสร้าง ฯลฯ แบบครบวงจรหาได้ที่นี่</p>
         </div>
         <div class="link">
             <h2>ลิงค์ที่น่าสนใจ</h2>
